e4398f55c656e63836a76176d7f1a889fcf6664f
Homero Ramirez <homeroram@outlook.com>
2024-02-13 18:12:43 -0600

    able to console log the returned json

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 console.log("Let's get this party started!");
+
+async function getGif(e) {
+    e.preventDefault();
+    let searchTerm = $('#term').val();
+    const gif = await axios.get("http://api.giphy.com/v1/gifs/search", {params:{q:"hilarious", api_key:"MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"}});
+    //console.log(gif);
+}  
+
+$('form').on('submit', getGif);
